dd90dca4d9a2b582bb332362a5bc5ec8ea2e7612
Yash <yashpatelit024@gmail.com>
2020-05-12 15:04:23 +0530

    updated homepage templates

---
+++ static/home.js
@@ -0,0 +1,9 @@
+var element = $(".element");
+
+$(function() {
+    element.typed({
+        strings: ["Web Designer.", "Graphic Designer.", "Freelancer."],
+        typeSpeed: 100,
+        loop: true,
+    });
+});
