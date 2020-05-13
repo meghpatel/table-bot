8adec95799bad4c6da92e815f0d6efcdd480afbb
meghpatel <patel.megh09@gmail.com>
2020-05-14 00:27:25 +0530

    Removed static/audio from gitignore

--- app.py
+++ app.py
@@ -275,6 +275,6 @@ def open_browser():
 	path = '/usr/bin/google-chrome %s --incognito'
 	webbrowser.get(path).open_new('http://127.0.0.1:5000')
 
-# if __name__ == '__main__':
-# 	Timer(1, open_browser).start()
-# 	app.run(debug=True) 
+if __name__ == '__main__':
+	Timer(1, open_browser).start()
+	app.run(debug=True) 
